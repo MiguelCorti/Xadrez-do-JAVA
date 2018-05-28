8acce92b16fc7e27d8e7b6ff159b40c66bc3b385
Miguel Corti <miguelszcorti@gmail.com>
2018-05-28 01:23:02 -0300

    Adding the board matrix and filling it with initial positions

--- src/Board.java
+++ src/Board.java
@@ -1,15 +1,56 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.Rectangle2D;
 import javax.swing.JPanel;
-import java.awt.*;
-import java.awt.geom.*;
 
 public class Board extends JPanel {
 	private final int SIZE=800;
 	private final int SQUARESIDE = SIZE/8;
 	
+	private Piece[][] bMatrix = new Piece[8][8];
+	
 	private static Board board = null;
 	
 	private Board()
 	{
+		// White Pieces
+		bMatrix[0][0] = new Rook(1, 1, 1);
+		bMatrix[0][1] = new Knight(1, 2, 1);
+		bMatrix[0][2] = new Bishop(1, 3, 1);
+		bMatrix[0][3] = new Queen(1, 4, 1);
+		bMatrix[0][4] = new King(1, 5, 1);
+		bMatrix[0][5] = new Bishop(1, 6, 1);
+		bMatrix[0][6] = new Knight(1, 7, 1);
+		bMatrix[0][7] = new Rook(1, 8, 1);
+		
+		bMatrix[1][0] = new Pawn(2, 1, 1);
+		bMatrix[1][1] = new Pawn(2, 2, 1);
+		bMatrix[1][2] = new Pawn(2, 3, 1);
+		bMatrix[1][3] = new Pawn(2, 4, 1);
+		bMatrix[1][4] = new Pawn(2, 5, 1);
+		bMatrix[1][5] = new Pawn(2, 6, 1);
+		bMatrix[1][6] = new Pawn(2, 7, 1);
+		bMatrix[1][7] = new Pawn(2, 8, 1);
+		
+		// Black Pieces
+		bMatrix[0][0] = new Rook(8, 1, -1);
+		bMatrix[0][1] = new Knight(8, 2, -1);
+		bMatrix[0][2] = new Bishop(8, 3, -1);
+		bMatrix[0][3] = new Queen(8, 4, -1);
+		bMatrix[0][4] = new King(8, 5, -1);
+		bMatrix[0][5] = new Bishop(8, 6, -1);
+		bMatrix[0][6] = new Knight(8, 7, -1);
+		bMatrix[0][7] = new Rook(8, 8, -1);
+		
+		bMatrix[1][0] = new Pawn(7, 1, -1);
+		bMatrix[1][1] = new Pawn(7, 2, -1);
+		bMatrix[1][2] = new Pawn(7, 3, -1);
+		bMatrix[1][3] = new Pawn(7, 4, -1);
+		bMatrix[1][4] = new Pawn(7, 5, -1);
+		bMatrix[1][5] = new Pawn(7, 6, -1);
+		bMatrix[1][6] = new Pawn(7, 7, -1);
+		bMatrix[1][7] = new Pawn(7, 8, -1);
 		
 	}
 	
@@ -26,7 +67,7 @@ public class Board extends JPanel {
 	
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		Graphics2D g2d=(Graphics2D) g;
+		Graphics2D g2d = (Graphics2D) g;
 		
 		int i, j;
 		
