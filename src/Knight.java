1c1623302aa3a3ef57142e303f11e94f23ee1d54
HandelSM <hscholzem@gmail.com>
2018-05-27 23:50:07 -0300

    Done knight class

--- src/Knight.java
+++ src/Knight.java
@@ -1,6 +1,10 @@
 
 public class Knight extends Piece{
 
+	public Knight(int row, int column, int color) {
+		super(row, column, color);
+	}
+
 	protected void updatePossiblePositions() {
 		possiblePositions.clear();
 		
@@ -29,7 +33,13 @@ public class Knight extends Piece{
 		
 		if(p.set(m_pos.getColumn()-2, m_pos.getRow()-1))
 			possiblePositions.add(p);
+		
+		for(int i = 0; i<possiblePositions.size(); i++)
+		{
+			if(Board.getBoard().sqrState(possiblePositions.get(i)) != 0)
+			{
+				possiblePositions.remove(i);
+			}
+		}
 	}
-
-
 }
