6c018c3baab378b3155d39f8d739a2836137edc7
HandelSM <hscholzem@gmail.com>
2018-06-23 05:33:53 -0300

    Fixing bugs

--- src/gameControl/King.java
+++ src/gameControl/King.java
@@ -11,42 +11,43 @@ public class King extends Piece{
 		
 		Position p = new Position();
 		if(p.set(m_pos.getRow()+0, m_pos.getColumn()+1))
-			possiblePositions.add(p);
+			if(Board.getBoard().sqrState(p) != getColor())
+				possiblePositions.add(p);
 		
 		p = new Position();
 		if(p.set(m_pos.getRow()+0, m_pos.getColumn()-1))
-			possiblePositions.add(p);
+			if(Board.getBoard().sqrState(p) != getColor())
+				possiblePositions.add(p);
 		
 		p = new Position();
 		if(p.set(m_pos.getRow()+1, m_pos.getColumn()+0))
-			possiblePositions.add(p);
+			if(Board.getBoard().sqrState(p) != getColor())
+				possiblePositions.add(p);
 		
 		p = new Position();
 		if(p.set(m_pos.getRow()-1, m_pos.getColumn()+0))
-			possiblePositions.add(p);
+			if(Board.getBoard().sqrState(p) != getColor())
+				possiblePositions.add(p);
 		
 		p = new Position();
 		if(p.set(m_pos.getRow()+1, m_pos.getColumn()+1))
-			possiblePositions.add(p);
+			if(Board.getBoard().sqrState(p) != getColor())
+				possiblePositions.add(p);
 		
 		p = new Position();
 		if(p.set(m_pos.getRow()-1, m_pos.getColumn()+1))
-			possiblePositions.add(p);
+			if(Board.getBoard().sqrState(p) != getColor())
+				possiblePositions.add(p);
 		
 		p = new Position();
 		if(p.set(m_pos.getRow()+1, m_pos.getColumn()-1))
-			possiblePositions.add(p);
+			if(Board.getBoard().sqrState(p) != getColor())
+				possiblePositions.add(p);
 		
 		p = new Position();
 		if(p.set(m_pos.getRow()-1, m_pos.getColumn()-1))
-			possiblePositions.add(p);
+			if(Board.getBoard().sqrState(p) != getColor())
+				possiblePositions.add(p);
 		
-		for(int i = 0; i<possiblePositions.size(); i++)
-		{
-			if(Board.getBoard().sqrState(possiblePositions.get(i)) != 0)
-			{
-				possiblePositions.remove(i);
-			}
-		}
 	}
 }
