f116b187cbf354ac22563ba787a58cf65fc15b5e
HandelSM <hscholzem@gmail.com>
2018-06-23 04:12:59 -0300

    Fixing some bugs

--- src/gameControl/Pawn.java
+++ src/gameControl/Pawn.java
@@ -24,22 +24,35 @@ public class Pawn extends Piece{
 		
 		Position p = new Position();
 		
-		if(p.set( m_pos.getRow()-getColor(), m_pos.getColumn() ))
+		if(p.set( m_pos.getRow()-getColor(), m_pos.getColumn() ) && Board.getBoard().sqrState(p) == 0 )
 			possiblePositions.add(p);
 		
 		if(m_pos.getRow() == 1 || m_pos.getRow()==2 || m_pos.getRow()==7 || m_pos.getRow()==8)
 		{
-			p = new Position();
-			if(p.set( m_pos.getRow()-2*getColor(), m_pos.getColumn() ))
-				possiblePositions.add(p);
-		}
-		
-		for(int i = 0; i<possiblePositions.size(); i++)
-		{
-			if(Board.getBoard().sqrState(possiblePositions.get(i)) != 0)
-			{
-				possiblePositions.remove(i);
+			if(!possiblePositions.isEmpty())
+			{				
+				p = new Position();
+				if(p.set( m_pos.getRow()-2*getColor(), m_pos.getColumn() ) && Board.getBoard().sqrState(p) == 0 )
+					possiblePositions.add(p);
 			}
 		}
+		
+		
+		/*p = new Position(m_pos.getRow() - getColor(), m_pos.getColumn() + 1);
+		if(Board.getBoard().sqrState(p) == (getColor()*(-1)))
+		{
+			possiblePositions.add(p);
+		}*/
+		
+		
+		/*
+		p = new Position(m_pos.getRow() - getColor(), m_pos.getColumn() - 1);
+		if(Board.getBoard().sqrState(p) == getColor()*(-1))
+		{
+			possiblePositions.add(p);
+		}*/
+			
+			
+			
 	}
 }
