d02c6b6844a4ed0c4d7acb63f7139c1173807df4
HandelSM <hscholzem@gmail.com>
2018-06-23 05:35:33 -0300

    Beginning to do pawn promotion

--- src/view/GameView.java
+++ src/view/GameView.java
@@ -14,6 +14,7 @@ import java.util.Observer;
 
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
 
 import gameControl.Bishop;
 import gameControl.Controller;
@@ -124,6 +125,10 @@ public class GameView extends JPanel implements Observer {
 					
 					squareColor[square.getRow()][square.getColumn()] = Color.RED;
 					break;
+				case 'P':
+					JPopupMenu promotionPopup;
+					promotionPopup = new JPopupMenu();
+					
 			}
 			
 			i++;
